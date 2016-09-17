5bda4dfcae3ed5a9f919f64698ad6c3ecef36616
Luan Santos <luankjs@gmail.com>
2016-09-17 13:04:34 -0300

    Adds BlackLib to project and testing in main file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
-#include "led.h"
+#include <unistd.h>
+#include "BlackLib/BlackGPIO/BlackGPIO.h"
+#include<iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
-  LED led_verde = LED(50);
+  BlackLib::BlackGPIO led_vermelho(BlackLib::GPIO_50,BlackLib::output, BlackLib::SecureMode);
 
-  string direction = led_verde.getDirection();
-  string value = led_verde.getValue();
+  if (led_verde.getDirection() == 2) {
+    cout << "O led verde está OUT" << endl;
+  } else {
+    cout << "Tem alguma coisa errada :(" << endl;
+  }
 
-  cout << "Direção: " << direction << endl;
-  cout << "Valor: " << value << endl;
+  sleep(5);
+
+  cout << "Ligando LED..." << endl;
+
+  led_verde.setValue(BlackLib::high);
+
+  if (led_verde.isHigh()) {
+    cout << "LED está ligado" << endl;
+  } else {
+    cout << "Tem alguma coisa errada :(" << endl;
+  }
+
+  sleep(5);
+
+  cout << "Desligando LED..." << endl;
+
+  led_verde.setValue(BlackLib::low);
+
+  if (led_verde.getValue() == "0") {
+    cout << "LED está desligado" << endl;
+  } else {
+    cout << "Tem alguma coisa errada :(" << endl;
+  }
 }
