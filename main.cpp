e1af2e790790fd8215513a092e0ade000eef73a3
Luan Santos <luankjs@gmail.com>
2016-09-26 07:47:41 -0300

    Genius game

--- main.cpp
+++ main.cpp
@@ -1,39 +1,143 @@
 #include <unistd.h>
 #include "BlackLib/BlackGPIO/BlackGPIO.h"
-#include<iostream>
+#include "color_selector.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
 
 using namespace std;
 
+BlackLib::BlackGPIO   led_red(BlackLib::GPIO_60, BlackLib::output, BlackLib::SecureMode);
+BlackLib::BlackGPIO   led_green(BlackLib::GPIO_30, BlackLib::output, BlackLib::SecureMode);
+BlackLib::BlackGPIO   button(BlackLib::GPIO_115, BlackLib::input, BlackLib::SecureMode);
+BlackLib::BlackGPIO   ledR(BlackLib::GPIO_50, BlackLib::output, BlackLib::SecureMode);
+BlackLib::BlackGPIO   ledG(BlackLib::GPIO_51, BlackLib::output, BlackLib::SecureMode);
+BlackLib::BlackGPIO   ledB(BlackLib::GPIO_4, BlackLib::output, BlackLib::SecureMode);
+ColorSelector         color_selector = ColorSelector(1);
+vector<string> colors_seq;
+vector<string> colors_choosen;
+string status_player;
+
+void highAll() {
+  ledR.setValue(BlackLib::high);
+  ledG.setValue(BlackLib::high);
+  ledB.setValue(BlackLib::high);
+}
+
+void lowAll() {
+  ledR.setValue(BlackLib::low);
+  ledG.setValue(BlackLib::low);
+  ledB.setValue(BlackLib::low);
+}
+
+string nextColor() {
+  int number = rand()%(3-1)+1;
+
+  if (number == 1) {
+    return "red";
+  }
+  else if (number == 2) {
+    return "green";
+  }
+  else if (number == 3) {
+    return "blue";
+  }
+  else {
+    return "red";
+  }
+}
+
+void printSequence() {
+  highAll();
+  sleep(1);
+  lowAll();
+  sleep(1);
+
+  for (int i = 0; i < colors_seq.size(); ++i) {
+    if (colors_seq.at(i) == "red") {
+      ledR.setValue(BlackLib::high);
+      ledG.setValue(BlackLib::low);
+      ledB.setValue(BlackLib::low);
+    } else if (colors_seq.at(i) == "green") {
+      ledR.setValue(BlackLib::low);
+      ledG.setValue(BlackLib::high);
+      ledB.setValue(BlackLib::low);
+    } else if (colors_seq.at(i) == "blue") {
+      ledR.setValue(BlackLib::low);
+      ledG.setValue(BlackLib::low);
+      ledB.setValue(BlackLib::high);
+    }
+    sleep(2);
+  }
+
+  highAll();
+  sleep(1);
+  lowAll();
+}
+
 int main(int argc, char* argv[]) {
-  BlackLib::BlackGPIO led_vermelho(BlackLib::GPIO_50,BlackLib::output, BlackLib::SecureMode);
+  status_player = "playing";
 
-  if (led_verde.getDirection() == 2) {
-    cout << "O led verde está OUT" << endl;
-  } else {
-    cout << "Tem alguma coisa errada :(" << endl;
+  while (status_player != "game_over") {
+    led_green.setValue(BlackLib::low);
+    led_red.setValue(BlackLib::low);
+    
+    colors_seq.push_back(nextColor());
+    
+    printSequence();
+
+    highAll();
+    sleep(1);
+    lowAll();
+
+    while(colors_choosen.size() != colors_seq.size()) {
+      if (button.getValue() == "1") {
+        if (color_selector.getAnalogValue() >= 0 && color_selector.getAnalogValue() < 1333) {
+          colors_choosen.push_back("red");
+        } else if (color_selector.getAnalogValue() >= 1333 && color_selector.getAnalogValue() < 2666) {
+          colors_choosen.push_back("green");
+        } else if (color_selector.getAnalogValue() >= 2666 && color_selector.getAnalogValue() < 4000) {
+          colors_choosen.push_back("blue");
+        }
+      } else {
+        if (color_selector.getAnalogValue() >= 0 && color_selector.getAnalogValue() < 1333) {
+          ledR.setValue(BlackLib::high);
+          ledG.setValue(BlackLib::low);
+          ledB.setValue(BlackLib::low);
+        } else if (color_selector.getAnalogValue() >= 1333 && color_selector.getAnalogValue() < 2666) {
+          ledR.setValue(BlackLib::low);
+          ledG.setValue(BlackLib::high);
+          ledB.setValue(BlackLib::low);
+        } else if (color_selector.getAnalogValue() >= 2666 && color_selector.getAnalogValue() < 4000) {
+          ledR.setValue(BlackLib::low);
+          ledG.setValue(BlackLib::low);
+          ledB.setValue(BlackLib::high);
+        }
+      }
+
+      sleep(0.5);
+    }
+
+    for (int i = 0; i < colors_seq.size(); ++i) {
+      if (colors_seq.at(i) != colors_choosen.at(i)) {
+        status_player = "game_over";
+      }
+    }
+
+    if (status_player != "game_over") {
+      led_green.setValue(BlackLib::high);
+      highAll();
+      sleep(1);
+      lowAll();
+      led_green.setValue(BlackLib::low);
+      colors_choosen.clear();
+    }
   }
 
-  sleep(5);
-
-  cout << "Ligando LED..." << endl;
-
-  led_verde.setValue(BlackLib::high);
-
-  if (led_verde.isHigh()) {
-    cout << "LED está ligado" << endl;
-  } else {
-    cout << "Tem alguma coisa errada :(" << endl;
-  }
-
-  sleep(5);
-
-  cout << "Desligando LED..." << endl;
-
-  led_verde.setValue(BlackLib::low);
-
-  if (led_verde.getValue() == "0") {
-    cout << "LED está desligado" << endl;
-  } else {
-    cout << "Tem alguma coisa errada :(" << endl;
-  }
+  led_red.setValue(BlackLib::high);
+  highAll();
+  sleep(1);
+  lowAll();
+  led_red.setValue(BlackLib::low);
 }
